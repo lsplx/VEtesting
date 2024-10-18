19d9403945e286222034385dff1e610ff6eab9ae
lsplx <zhiyuan2019@iscas.ac.cn>
2024-10-18 10:48:48 +0800

    add

--- extractentity_VE.py
+++ extractentity_VE.py
@@ -14,105 +14,6 @@ def edit_distance_score(text1, text2):
 
 openai.api_key = "your_apikey"
 
-def compute_metrics_subject(predictions, labels):
-    tp = fp = fn = 0
-    fp_flag = 0
-    for prediction, label in zip(predictions, labels):
-        for each in label:
-            if edit_distance_score(prediction.strip(),each) > 0.8:
-                tp += 1
-                fp_flag = 1
-            else:
-                fn += 1
-    if fp_flag == 0:
-        fp += 1
-        # if prediction and label:  # True Positive
-        #     tp += 1
-        # elif prediction and not label:  # False Positive
-        #     fp += 1
-        # elif not prediction and label:  # False Negative
-        #     fn += 1
-    precision = tp / (tp + fp) if tp + fp > 0 else 0
-    recall = tp / (tp + fn) if tp + fn > 0 else 0
-    f1 = 2 * (precision * recall) / (precision + recall) if precision + recall > 0 else 0
-    return precision, recall, f1
-
-def compute_metrics_modifier(predictions, labels):
-    tp = fp = fn = 0
-    for prediction, label in zip(predictions, labels):
-        for each in prediction:
-            fp_flag = 0
-            for every in label:
-                if  edit_distance_score(each.strip(),every) > 0.6:
-                    tp += 1
-                    fp_flag = 1
-            if fp_flag == 0:
-                fp += 1
-        for each in label:
-            fn_flag = 0
-            for every in prediction:    
-                if edit_distance_score(each,every.strip()) > 0.6:
-                    fn_flag = 1
-            if fn_flag == 0:
-                fn += 1
-    precision = tp / (tp + fp) if tp + fp > 0 else 0
-    recall = tp / (tp + fn) if tp + fn > 0 else 0
-    f1 = 2 * (precision * recall) / (precision + recall) if precision + recall > 0 else 0
-    return precision, recall, f1
-
-
-def evaluate_model(predictions, test_data):
-    subjects_predictions = [p['subject'] for p in predictions]
-    subjects_labels = []
-    for data in test_data:
-        templist = []
-        for entity in data["entities"]:
-            if  entity['type'] == '主体':
-                templist.append(entity['entity'].strip())
-        subjects_labels.append(templist)
-    modifiers_predictions = [p['modifiers'] for p in predictions]
-    modifiers_labels = []
-    for data in test_data:
-        templist = []
-        for entity in data["entities"]:
-            if  entity['type'] == '特征':
-                templist.append(entity['entity'].strip())
-        modifiers_labels.append(templist)
-
-    subject_precision, subject_recall, subject_f1 = compute_metrics_subject(subjects_predictions, subjects_labels)
-    modifier_precision, modifier_recall, modifier_f1 = compute_metrics_modifier(modifiers_predictions, modifiers_labels)
-
-    final_list = []
-    for num,each in enumerate(subjects_predictions) :
-        templist = []
-        combined_string = ' '.join(test_data[num]['tokens'])
-        templist.append(combined_string)
-        templist.append(each)
-        subjectstring_gth = ""
-        for subject in subjects_labels[num]:
-            subjectstring_gth =subjectstring_gth + "; " +  subject 
-        templist.append(subjectstring_gth)
-        modifierstring = ""
-        for modifier in modifiers_predictions[num]:
-            modifierstring = modifierstring+ "; " + modifier  
-        templist.append(modifierstring)
-        modifierstring_gth = ""
-        for modifier_gth in modifiers_labels[num]:
-            modifierstring_gth = modifierstring_gth + "; " + modifier_gth 
-        templist.append(modifierstring_gth)
-        final_list.append(templist)
-    # 创建一个新的 Excel 工作簿
-    df = DataFrame(final_list,columns=["sentence","sub_predict","sub_gth","modifier_predict","modifier_gth"])
-    df.to_excel('D:/test400_predictgpt_nocotext.xlsx', sheet_name='Sheet1', index=False)
-
-
-    print("Subject Precision:", subject_precision)
-    print("Subject Recall:", subject_recall)
-    print("Subject F1:", subject_f1)
-    print("Modifier Precision:", modifier_precision)
-    print("Modifier Recall:", modifier_recall)
-    print("Modifier F1:", modifier_f1)
-
 
 def load_test_data(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -288,28 +189,17 @@ def extract_entities(uniq_list,hypothesis_list,label_list):
         for each in output_json:
             subject_new = subject_new + ";" +  each["subject"]
             modifier_new = modifier_new + ";" + each["modifier"]
-        # templist.append(uniq_list[num])
-        # templist.append(subject)
-        # templist.append(modifier)
-        # templist.append(label_list[num])
-        # final_list.append(templist)
 
-        object_path = "D:/esnli_test_entity.xlsx"
+        object_path = "D:/snli_test_entity.xlsx"
         try:
             pd_sheets = pd.ExcelFile(object_path)
         except Exception as e:
-            print("读取 {} 文件失败".format(object_path), e)
+            print("read {} file fail".format(object_path), e)
         df = pd.read_excel(pd_sheets, "Sheet1", header=[0])
-        # uniq_list = [] 
-        # subject_list = []
-        # modifier_list = []
-        # label_list = []
-        # num = 0
         final_list = []
         for row in df.itertuples(index=True):
             templist = []
             row_list = list(row)
-            #uniq_id
             uniq_id = row_list[1:2]
             subject = row_list[2:3]
             modifier = row_list[3:4]
@@ -329,17 +219,14 @@ def extract_entities(uniq_list,hypothesis_list,label_list):
         templist.append(sentence)
         final_list.append(templist)
         df1 = DataFrame(final_list,columns=["uniq_id","subject","modifier","label","sentence"])
-        df1.to_excel('D:/esnli_test_entity.xlsx', sheet_name='Sheet1', index=False)        
+        df1.to_excel('D:/snli_test_entity.xlsx', sheet_name='Sheet1', index=False)        
     return predictions
 
-
-
-
-object_path = "D:/esnli_ve_test.xlsx"
+object_path = "D:/snli_ve_test.xlsx"
 try:
     pd_sheets = pd.ExcelFile(object_path)
 except Exception as e:
-    print("读取 {} 文件失败".format(object_path), e)
+    print("read {} file fail".format(object_path), e)
 df = pd.read_excel(pd_sheets, "Sheet1", header=[0])
 uniq_list = [] 
 hypothesis_list = []
@@ -349,7 +236,6 @@ num = 0
 final_list = []
 for row in df.itertuples(index=True):
     row_list = list(row)
-
     uniq_id = row_list[1:2]
     hypothesis = row_list[2:3]
     caption = row_list[3:4]
@@ -357,8 +243,6 @@ for row in df.itertuples(index=True):
     uniq_list.append(uniq_id[0])
     hypothesis_list.append(hypothesis[0])
     label_list.append(label[0])
-
-# test_data = [{"tokens": ["the", "red", "luggage"], "entities": [{"entity": "luggage ", "start": 2, "end": 3, "type": "subject", "newid": 0, "id": 0}, {"entity": "red ", "start": 1, "end": 2, "type": "feature", "newid": 1, "id": 1}], "relations": []}]
 predictions = extract_entities(uniq_list,hypothesis_list,label_list)
 
 
